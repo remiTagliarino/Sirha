eecb65b3556f0309b2ba653e7836442b40b4772d
remiTagliarino <remi.tagliarino@gmail.com>
2016-12-21 09:38:51 +0100

    Initial commit

---
+++ Sirha.UnitTest/AccueilUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sirha.ViewModel;
+
+namespace Sirha.UnitTest
+{
+    [TestClass]
+    public class AccueilUnitTest
+    {
+        [TestMethod]
+        [TestCategory("CanSearch")]
+        public void CanSearch_AllVariableEmpty_Test()
+        {
+            //Initialisation variable
+            var vm = new AccueilViewModel();
+            vm.Adresse = "";
+            vm.CodePostal = "";
+            vm.Commercial = "";
+            vm.Nom = "";
+            vm.Region = "";
+            vm.Ville = "";
+            //éxécution méthode
+            var result = vm.CanSearch();
+            //test
+            Assert.IsTrue(result == false, "La fonction doit retournée false");
+        }
+
+        [TestMethod]
+        [TestCategory("CanSearch")]
+        public void CanSearch_OneVariableNotEmpty_Test()
+        {
+            //Initialisation variable
+            var vm = new AccueilViewModel();
+            vm.Adresse = "mlamzl";
+            vm.CodePostal = "";
+            vm.Commercial = "";
+            vm.Nom = "";
+            vm.Region = "";
+            vm.Ville = "";
+            //éxécution méthode
+            var result = vm.CanSearch();
+            //test
+            Assert.IsTrue(result == true, "La fonction doit retournée true");
+        }
+    }
+}
